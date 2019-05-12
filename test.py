3c0d5934b93ad7aa37733aff7a220cdf7c5bf406
tallapanenirambabu332 <39071911+tallapanenirambabu332@users.noreply.github.com>
2019-05-12 18:57:45 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import yaml
 with open('repos.yml', 'r') as f:
    doc = yaml.load(f)
 print(doc)
-for repo_name in doc[‘repos’]:   
-   name = doc[‘repos’][repo_name]
+for repo_name in doc[‘repos_list’]:   
+   name = doc[‘repos_list’][repo_name]
    print(name) 
