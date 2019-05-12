48258b278a98dc5226f91669f33369e3504c62e9
tallapanenirambabu332 <39071911+tallapanenirambabu332@users.noreply.github.com>
2019-05-12 19:43:32 -0400

    Update ram.py

--- ram.py
+++ ram.py
@@ -1,13 +1,13 @@
 from ruamel.yaml import YAML
-yaml = YAML()
-input_file = 'repos.yml'
-repos_list = []
-i = 0
 def parseYaml():
+  yaml = YAML()
+  input_file = 'repos.yml'
+  repos_list = []
+  i = 0
   for key, value in yaml.load(open(input_file)).items():
     print(str(value))
     i+=1
     repos_list.insert(i,value)
     print(repos_list)
     return repos_list
-parseYaml()
+  parseYaml()
