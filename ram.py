fb3995b8f28f92771e53df5497cef87ff4f33772
tallapanenirambabu332 <39071911+tallapanenirambabu332@users.noreply.github.com>
2019-05-12 19:41:17 -0400

    Update ram.py

--- ram.py
+++ ram.py
@@ -3,9 +3,11 @@ yaml = YAML()
 input_file = 'repos.yml'
 repos_list = []
 i = 0
-for key, value in yaml.load(open(input_file)).items():
-  print(str(value))
-  i+=1
-  repos_list.insert(i,value)
-  print(repos_list)
-  return repos_list
+def parseYaml():
+  for key, value in yaml.load(open(input_file)).items():
+    print(str(value))
+    i+=1
+    repos_list.insert(i,value)
+    print(repos_list)
+    return repos_list
+parseYaml()
