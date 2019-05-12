ba0539faa407e13fc1e91747e63d4ff354db033a
tallapanenirambabu332 <39071911+tallapanenirambabu332@users.noreply.github.com>
2019-05-12 19:33:05 -0400

    Update ram.py

--- ram.py
+++ ram.py
@@ -1,5 +1,10 @@
 from ruamel.yaml import YAML
 yaml = YAML()
 input_file = 'repos.yml'
+repos_list = []
+i = 0
 for key, value in yaml.load(open(input_file)).items():
   print(str(value))
+  i++
+  repos_list.insert(i,value)
+  print(repos_list)
