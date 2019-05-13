9b11ea017fabcc9f3d97f6ac2f64cc526620eceb
tallapanenirambabu332 <39071911+tallapanenirambabu332@users.noreply.github.com>
2019-05-13 14:51:26 -0400

    Update ram.py

--- ram.py
+++ ram.py
@@ -1,18 +1,15 @@
-from ruamel.yaml import YAML
+from ruamel.yaml
+import YAML
 def parseYaml():
   yaml = YAML()
-  input_file = 'repos.yml'
-  repos_list = []
-  i = 0
-  for key, value in yaml.load(open(input_file)).items():
-    print(str(value))
-    i+=1
-    repos_list.insert(i,value)
-    #print(repos_list)
-  #return repos_list
-  with open("output.txt","w") as f:
-    for item in repos_list:
-      f.write("%s\n" %item)
-#repositories = parseYaml()
-parseYaml()
-#print(repositories)
+input_file = 'repos.yml'
+repos_list = []
+i = 0
+for key, value in yaml.load(open(input_file)).items():
+  print(str(value))
+i += 1
+repos_list.insert(i, value)# print(repos_list)# return repos_list
+with open("output.txt", "w") as f:
+  for item in repos_list:
+  f.write("%s\n" % item)# repositories = parseYaml()
+parseYaml()# print(repositories)
